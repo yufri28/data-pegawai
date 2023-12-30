818e13a2f42fc743a6ac597001da674e6979d5c7
Yufri Luttu <yufridonluttu@gmail.com>
2023-12-30 16:17:29 +0800

    ekspor database terbaru

--- datapegawai.sql
+++ datapegawai.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 6.0.0-dev+20231112.f9ab9d15ac
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 11 Des 2023 pada 08.38
--- Versi server: 10.4.14-MariaDB
--- Versi PHP: 7.2.34
+-- Generation Time: Dec 30, 2023 at 09:16 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `data_golongan`
+-- Table structure for table `data_golongan`
 --
 
 CREATE TABLE `data_golongan` (
@@ -36,7 +36,7 @@ CREATE TABLE `data_golongan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `data_golongan`
+-- Dumping data for table `data_golongan`
 --
 
 INSERT INTO `data_golongan` (`id_golongan`, `golongan`, `tmt`, `jumlah_gaji`, `id_pegawai`) VALUES
@@ -46,7 +46,7 @@ INSERT INTO `data_golongan` (`id_golongan`, `golongan`, `tmt`, `jumlah_gaji`, `i
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `data_jabatan`
+-- Table structure for table `data_jabatan`
 --
 
 CREATE TABLE `data_jabatan` (
@@ -58,7 +58,7 @@ CREATE TABLE `data_jabatan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `data_jabatan`
+-- Dumping data for table `data_jabatan`
 --
 
 INSERT INTO `data_jabatan` (`id_jabatan`, `nama`, `eselon`, `tmt`, `id_pegawai`) VALUES
@@ -68,7 +68,7 @@ INSERT INTO `data_jabatan` (`id_jabatan`, `nama`, `eselon`, `tmt`, `id_pegawai`)
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `data_mutasi`
+-- Table structure for table `data_mutasi`
 --
 
 CREATE TABLE `data_mutasi` (
@@ -79,7 +79,7 @@ CREATE TABLE `data_mutasi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `data_mutasi`
+-- Dumping data for table `data_mutasi`
 --
 
 INSERT INTO `data_mutasi` (`id_mutasi`, `id_pegawai`, `tempat_mutasi`, `jenis_mutasi`) VALUES
@@ -88,7 +88,7 @@ INSERT INTO `data_mutasi` (`id_mutasi`, `id_pegawai`, `tempat_mutasi`, `jenis_mu
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `data_pegawai`
+-- Table structure for table `data_pegawai`
 --
 
 CREATE TABLE `data_pegawai` (
@@ -102,21 +102,23 @@ CREATE TABLE `data_pegawai` (
   `jk` enum('Laki-Laki','Perempuan','Tidak diketahui') NOT NULL,
   `agama` enum('Kristen','Islam','Katolik','Hindu','Buddha','Khonghucu') NOT NULL,
   `status` enum('Tetap','Honor') NOT NULL,
-  `id_periode` int(11) NOT NULL
+  `id_periode` int(11) NOT NULL,
+  `skpp` date DEFAULT NULL,
+  `skpt` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `data_pegawai`
+-- Dumping data for table `data_pegawai`
 --
 
-INSERT INTO `data_pegawai` (`id_pegawai`, `nip`, `nama`, `alamat`, `no_hp`, `tempat_lahir`, `tanggal_lahir`, `jk`, `agama`, `status`, `id_periode`) VALUES
-(1308, '197004291994031005', 'Nixon M Balukh', 'wadgjuk', '1234567890', 'kupang', '2023-11-10', 'Laki-Laki', 'Kristen', 'Tetap', 17),
-(1309, '111111111111111123', 'adit', 'awesrdgtfhygjukh', '099786654', 'tdejetde', '2023-11-10', 'Perempuan', 'Khonghucu', 'Honor', 13);
+INSERT INTO `data_pegawai` (`id_pegawai`, `nip`, `nama`, `alamat`, `no_hp`, `tempat_lahir`, `tanggal_lahir`, `jk`, `agama`, `status`, `id_periode`, `skpp`, `skpt`) VALUES
+(1308, '197004291994031005', 'Nixon M Balukh', 'wadgjuk', '1234567890', 'kupang', '2023-11-10', 'Laki-Laki', 'Kristen', 'Tetap', 17, NULL, NULL),
+(1309, '111111111111111123', 'adit', 'awesrdgtfhygjukh', '099786654', 'tdejetde', '2023-11-10', 'Perempuan', 'Khonghucu', 'Honor', 13, NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `data_pendidikan`
+-- Table structure for table `data_pendidikan`
 --
 
 CREATE TABLE `data_pendidikan` (
@@ -131,20 +133,20 @@ CREATE TABLE `data_pendidikan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `data_pendidikan`
+-- Dumping data for table `data_pendidikan`
 --
 
 INSERT INTO `data_pendidikan` (`id_pendidikan`, `id_pegawai`, `tahun_lulus`, `jurusan`, `pendidikan_terakhir`, `lembaga_pendidikan`, `kursus_diklat`, `pend_perjenjangan`) VALUES
-(2, 1302, 2008, 'Kimia', 'S1', 'Undana', '', ''),
-(3, 1303, 2008, 'Kimia', 'S1', 'Undana', '', ''),
-(4, 1306, 2012, 'Kimia', 'S1', 'Undana', '', ''),
-(6, 1309, 2009, 'Pertanian', 'SMA', 'Undana', '', ''),
-(7, 1308, 2018, 'Ilmu Komputer', 'S1', 'Universitas Nusa Cendana', 'Kursus Bahasa Inggris, Kursus Bahasa Jerman dan Kursus Komputer', 'Pra Identifikasi Klpk sasaran PPL, Agribisnis Ternak Potong dan Teknologi  Lahan');
+(2, 1302, '2008', 'Kimia', 'S1', 'Undana', '', ''),
+(3, 1303, '2008', 'Kimia', 'S1', 'Undana', '', ''),
+(4, 1306, '2012', 'Kimia', 'S1', 'Undana', '', ''),
+(6, 1309, '2009', 'Pertanian', 'SMA', 'Undana', '', ''),
+(7, 1308, '2018', 'Ilmu Komputer', 'S1', 'Universitas Nusa Cendana', 'Kursus Bahasa Inggris, Kursus Bahasa Jerman dan Kursus Komputer', 'Pra Identifikasi Klpk sasaran PPL, Agribisnis Ternak Potong dan Teknologi  Lahan');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `data_pensiun`
+-- Table structure for table `data_pensiun`
 --
 
 CREATE TABLE `data_pensiun` (
@@ -155,16 +157,16 @@ CREATE TABLE `data_pensiun` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `data_pensiun`
+-- Dumping data for table `data_pensiun`
 --
 
 INSERT INTO `data_pensiun` (`id_pensiun`, `id_pegawai`, `tahun_pensiun`, `jabatan_terakhir`) VALUES
-(10, 1308, 2028, 'Kabid TPH');
+(10, 1308, '2028', 'Kabid TPH');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `periode`
+-- Table structure for table `periode`
 --
 
 CREATE TABLE `periode` (
@@ -173,17 +175,17 @@ CREATE TABLE `periode` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `periode`
+-- Dumping data for table `periode`
 --
 
 INSERT INTO `periode` (`id_periode`, `tahun`) VALUES
-(13, 2023),
-(17, 2024);
+(13, '2023'),
+(17, '2024');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_pengguna`
+-- Table structure for table `tb_pengguna`
 --
 
 CREATE TABLE `tb_pengguna` (
@@ -195,7 +197,7 @@ CREATE TABLE `tb_pengguna` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tb_pengguna`
+-- Dumping data for table `tb_pengguna`
 --
 
 INSERT INTO `tb_pengguna` (`id_pengguna`, `nama_pengguna`, `username`, `password`, `level`) VALUES
@@ -206,7 +208,7 @@ INSERT INTO `tb_pengguna` (`id_pengguna`, `nama_pengguna`, `username`, `password
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_profil`
+-- Table structure for table `tb_profil`
 --
 
 CREATE TABLE `tb_profil` (
@@ -217,7 +219,7 @@ CREATE TABLE `tb_profil` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tb_profil`
+-- Dumping data for table `tb_profil`
 --
 
 INSERT INTO `tb_profil` (`id_profil`, `nama_profil`, `alamat`, `bidang`) VALUES
@@ -228,42 +230,42 @@ INSERT INTO `tb_profil` (`id_profil`, `nama_profil`, `alamat`, `bidang`) VALUES
 --
 
 --
--- Indeks untuk tabel `data_golongan`
+-- Indexes for table `data_golongan`
 --
 ALTER TABLE `data_golongan`
   ADD PRIMARY KEY (`id_golongan`),
   ADD KEY `id_pegawai` (`id_pegawai`);
 
 --
--- Indeks untuk tabel `data_jabatan`
+-- Indexes for table `data_jabatan`
 --
 ALTER TABLE `data_jabatan`
   ADD PRIMARY KEY (`id_jabatan`),
   ADD KEY `id_pegawai` (`id_pegawai`);
 
 --
--- Indeks untuk tabel `data_mutasi`
+-- Indexes for table `data_mutasi`
 --
 ALTER TABLE `data_mutasi`
   ADD PRIMARY KEY (`id_mutasi`),
   ADD KEY `id_pegawai` (`id_pegawai`);
 
 --
--- Indeks untuk tabel `data_pegawai`
+-- Indexes for table `data_pegawai`
 --
 ALTER TABLE `data_pegawai`
   ADD PRIMARY KEY (`id_pegawai`),
   ADD KEY `id_periode` (`id_periode`);
 
 --
--- Indeks untuk tabel `data_pendidikan`
+-- Indexes for table `data_pendidikan`
 --
 ALTER TABLE `data_pendidikan`
   ADD PRIMARY KEY (`id_pendidikan`),
   ADD KEY `id_pegawai` (`id_pegawai`);
 
 --
--- Indeks untuk tabel `data_pensiun`
+-- Indexes for table `data_pensiun`
 --
 ALTER TABLE `data_pensiun`
   ADD PRIMARY KEY (`id_pensiun`),
@@ -271,105 +273,105 @@ ALTER TABLE `data_pensiun`
   ADD KEY `id_pegawai` (`id_pegawai`);
 
 --
--- Indeks untuk tabel `periode`
+-- Indexes for table `periode`
 --
 ALTER TABLE `periode`
   ADD PRIMARY KEY (`id_periode`);
 
 --
--- Indeks untuk tabel `tb_pengguna`
+-- Indexes for table `tb_pengguna`
 --
 ALTER TABLE `tb_pengguna`
   ADD PRIMARY KEY (`id_pengguna`);
 
 --
--- Indeks untuk tabel `tb_profil`
+-- Indexes for table `tb_profil`
 --
 ALTER TABLE `tb_profil`
   ADD PRIMARY KEY (`id_profil`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `data_golongan`
+-- AUTO_INCREMENT for table `data_golongan`
 --
 ALTER TABLE `data_golongan`
   MODIFY `id_golongan` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT untuk tabel `data_jabatan`
+-- AUTO_INCREMENT for table `data_jabatan`
 --
 ALTER TABLE `data_jabatan`
   MODIFY `id_jabatan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `data_mutasi`
+-- AUTO_INCREMENT for table `data_mutasi`
 --
 ALTER TABLE `data_mutasi`
   MODIFY `id_mutasi` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT untuk tabel `data_pegawai`
+-- AUTO_INCREMENT for table `data_pegawai`
 --
 ALTER TABLE `data_pegawai`
-  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1310;
+  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1314;
 
 --
--- AUTO_INCREMENT untuk tabel `data_pendidikan`
+-- AUTO_INCREMENT for table `data_pendidikan`
 --
 ALTER TABLE `data_pendidikan`
   MODIFY `id_pendidikan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT untuk tabel `data_pensiun`
+-- AUTO_INCREMENT for table `data_pensiun`
 --
 ALTER TABLE `data_pensiun`
   MODIFY `id_pensiun` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT untuk tabel `periode`
+-- AUTO_INCREMENT for table `periode`
 --
 ALTER TABLE `periode`
   MODIFY `id_periode` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_pengguna`
+-- AUTO_INCREMENT for table `tb_pengguna`
 --
 ALTER TABLE `tb_pengguna`
   MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_profil`
+-- AUTO_INCREMENT for table `tb_profil`
 --
 ALTER TABLE `tb_profil`
   MODIFY `id_profil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `data_golongan`
+-- Constraints for table `data_golongan`
 --
 ALTER TABLE `data_golongan`
   ADD CONSTRAINT `data_golongan_ibfk_1` FOREIGN KEY (`id_pegawai`) REFERENCES `data_pegawai` (`id_pegawai`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `data_mutasi`
+-- Constraints for table `data_mutasi`
 --
 ALTER TABLE `data_mutasi`
   ADD CONSTRAINT `data_mutasi_ibfk_1` FOREIGN KEY (`id_pegawai`) REFERENCES `data_pegawai` (`id_pegawai`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `data_pegawai`
+-- Constraints for table `data_pegawai`
 --
 ALTER TABLE `data_pegawai`
   ADD CONSTRAINT `data_pegawai_ibfk_1` FOREIGN KEY (`id_periode`) REFERENCES `periode` (`id_periode`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `data_pensiun`
+-- Constraints for table `data_pensiun`
 --
 ALTER TABLE `data_pensiun`
   ADD CONSTRAINT `data_pensiun_ibfk_1` FOREIGN KEY (`id_pegawai`) REFERENCES `data_pegawai` (`id_pegawai`) ON DELETE CASCADE ON UPDATE CASCADE;
